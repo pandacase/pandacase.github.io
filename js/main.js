71e4e0c79c77e71af2833336d47157d4443f1e3d
panda <2227119810@qq.com>
2023-04-25 16:29:03 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@
 
 
 	// Contact 文本复制器
-	const eletments = document.getElementsByClassName("copy-link"); // 获取要复制的文本
+	const eletments = document.getElementsByClassName("copy-link");
 	const array = Array.from(eletments);
 
 	array.forEach(function(element){
