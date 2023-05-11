5907963ce6a44339e23f0f39d41f145cc436dac5
panda <2227119810@qq.com>
2023-05-11 19:03:44 +0800

    Update highlight.js

--- js/highlight.js
+++ js/highlight.js
@@ -1,24 +1,43 @@
-// onload:
-function initCodeHighlight(){
-    document.body.classList.add("line-numbers","match-braces");
-    Prism.plugins.toolbar.registerButton("select-code", function (env) {
-    let button = document.createElement("button");
-    button.textContent = "select this " + env.language;
-    button.addEventListener("click", function () {
-        if (document.body.createTextRange) {
-        let range = document.body.createTextRange();
-        range.moveToElementText(env.element);
-        range.select();
-        } else if (window.getSelection) {
-        let selection = window.getSelection();
-        let range = document.createRange();
-        range.selectNodeContents(env.element);
-        selection.removeAllRanges();
-        selection.addRange(range);
+const MarkdownIt = require('markdown-it')
+const hljs = require('highlight.js')
+const md = new MarkdownIt({
+  html: true,
+  linkify: true,
+  typographer: true,
+  highlight: function (str, lang) {
+  	// 此处判断是否有添加代码语言
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+      	// 得到经过highlight.js之后的html代码
+        const preCode = hljs.highlight(lang, str, true).value
+        // 以换行进行分割
+        const lines = preCode.split(/\n/).slice(0, -1)
+        // 添加自定义行号
+        let html = lines.map((item, index) => {
+          return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
+        }).join('')
+        html = '<ol>' + html + '</ol>'
+        // 添加代码语言
+        if (lines.length) {
+          html += '<b class="name">' + lang + '</b>'
         }
-    });
-    return button;
-    });
-}
+        return '<pre class="hljs"><code>' +
+          html +
+          '</code></pre>'
+      } catch (__) {}
+    }
+	// 未添加代码语言，此处与上面同理
+    const preCode = md.utils.escapeHtml(str)
+    const lines = preCode.split(/\n/).slice(0, -1)
+    let html = lines.map((item, index) => {
+      return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
+    }).join('')
+    html = '<ol>' + html + '</ol>'
+    return '<pre class="hljs"><code>' +
+      html +
+      '</code></pre>'
+  }
+})
 
-window.onload = initCodeHighlight;
+// req.body.content 代表md代码
+let articleContentHtml = md.render(`@[toc]${req.body.content}`)
