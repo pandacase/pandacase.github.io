2fc23b975ff94087c39a88a3fc0aa7693376fd1a
panda <99351752+pandacase@users.noreply.github.com>
2023-11-16 20:58:27 +0800

    Update highlight.js

--- js/highlight.js
+++ js/highlight.js
@@ -1,43 +1,43 @@
-const MarkdownIt = require('markdown-it')
-const hljs = require('highlight.js')
-const md = new MarkdownIt({
-  html: true,
-  linkify: true,
-  typographer: true,
-  highlight: function (str, lang) {
-  	// 此处判断是否有添加代码语言
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-      	// 得到经过highlight.js之后的html代码
-        const preCode = hljs.highlight(lang, str, true).value
-        // 以换行进行分割
-        const lines = preCode.split(/\n/).slice(0, -1)
-        // 添加自定义行号
-        let html = lines.map((item, index) => {
-          return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
-        }).join('')
-        html = '<ol>' + html + '</ol>'
-        // 添加代码语言
-        if (lines.length) {
-          html += '<b class="name">' + lang + '</b>'
-        }
-        return '<pre class="hljs"><code>' +
-          html +
-          '</code></pre>'
-      } catch (__) {}
-    }
-	// 未添加代码语言，此处与上面同理
-    const preCode = md.utils.escapeHtml(str)
-    const lines = preCode.split(/\n/).slice(0, -1)
-    let html = lines.map((item, index) => {
-      return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
-    }).join('')
-    html = '<ol>' + html + '</ol>'
-    return '<pre class="hljs"><code>' +
-      html +
-      '</code></pre>'
-  }
-})
+// const MarkdownIt = require('markdown-it')
+// const hljs = require('highlight.js')
+// const md = new MarkdownIt({
+//   html: true,
+//   linkify: true,
+//   typographer: true,
+//   highlight: function (str, lang) {
+//   	// 此处判断是否有添加代码语言
+//     if (lang && hljs.getLanguage(lang)) {
+//       try {
+//       	// 得到经过highlight.js之后的html代码
+//         const preCode = hljs.highlight(lang, str, true).value
+//         // 以换行进行分割
+//         const lines = preCode.split(/\n/).slice(0, -1)
+//         // 添加自定义行号
+//         let html = lines.map((item, index) => {
+//           return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
+//         }).join('')
+//         html = '<ol>' + html + '</ol>'
+//         // 添加代码语言
+//         if (lines.length) {
+//           html += '<b class="name">' + lang + '</b>'
+//         }
+//         return '<pre class="hljs"><code>' +
+//           html +
+//           '</code></pre>'
+//       } catch (__) {}
+//     }
+// 	// 未添加代码语言，此处与上面同理
+//     const preCode = md.utils.escapeHtml(str)
+//     const lines = preCode.split(/\n/).slice(0, -1)
+//     let html = lines.map((item, index) => {
+//       return '<li><span class="line-num" data-line="' + (index + 1) + '"></span>' + item + '</li>'
+//     }).join('')
+//     html = '<ol>' + html + '</ol>'
+//     return '<pre class="hljs"><code>' +
+//       html +
+//       '</code></pre>'
+//   }
+// })
 
-// req.body.content 代表md代码
-let articleContentHtml = md.render(`@[toc]${req.body.content}`)
+// // req.body.content 代表md代码
+// let articleContentHtml = md.render(`@[toc]${req.body.content}`)
