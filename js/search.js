d3b95d1d823b8c6014be368b8f06b1ca518da384
panda <99351752+pandacase@users.noreply.github.com>
2023-11-16 20:59:19 +0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,65 +1,65 @@
-$(function() {
-    var $searchIcon = $('.search-icon');
-    var $searchBox = $('<div class="search-box"><form><input type="text" placeholder="Search"></form></div>');
-    var $searchInput = $searchBox.find('input[type="text"]');
-    var $searchResults = $('<div class="search-results"></div>');
-    var $body = $('body');
+// $(function() {
+//     var $searchIcon = $('.search-icon');
+//     var $searchBox = $('<div class="search-box"><form><input type="text" placeholder="Search"></form></div>');
+//     var $searchInput = $searchBox.find('input[type="text"]');
+//     var $searchResults = $('<div class="search-results"></div>');
+//     var $body = $('body');
   
-    $searchIcon.after($searchBox);
+//     $searchIcon.after($searchBox);
   
-    $searchInput.on('input', function() {
-      var query = $(this).val().toLowerCase();
-      var results = [];
+//     $searchInput.on('input', function() {
+//       var query = $(this).val().toLowerCase();
+//       var results = [];
   
-      if (query) {
-        $('article').each(function() {
-          var $article = $(this);
-          var title = $article.find('.post-title').text().toLowerCase();
-          var content = $article.find('.post-content').text().toLowerCase();
+//       if (query) {
+//         $('article').each(function() {
+//           var $article = $(this);
+//           var title = $article.find('.post-title').text().toLowerCase();
+//           var content = $article.find('.post-content').text().toLowerCase();
   
-          if (title.indexOf(query) !== -1 || content.indexOf(query) !== -1) {
-            results.push({
-              title: title,
-              content: content,
-              url: $article.find('a').attr('href')
-            });
-          }
-        });
-      }
+//           if (title.indexOf(query) !== -1 || content.indexOf(query) !== -1) {
+//             results.push({
+//               title: title,
+//               content: content,
+//               url: $article.find('a').attr('href')
+//             });
+//           }
+//         });
+//       }
   
-      displaySearchResults(results);
-    });
+//       displaySearchResults(results);
+//     });
   
-    function displaySearchResults(results) {
-      $searchResults.empty();
+//     function displaySearchResults(results) {
+//       $searchResults.empty();
   
-      if (results.length) {
-        results.forEach(function(result) {
-          var $result = $('<div><a href="' + result.url + '">' + result.title + '</a></div>');
-          $searchResults.append($result);
-        });
-      } else {
-        $searchResults.html('<p>No results found.</p>');
-      }
+//       if (results.length) {
+//         results.forEach(function(result) {
+//           var $result = $('<div><a href="' + result.url + '">' + result.title + '</a></div>');
+//           $searchResults.append($result);
+//         });
+//       } else {
+//         $searchResults.html('<p>No results found.</p>');
+//       }
   
-      $searchResults.appendTo($body).show();
-    }
+//       $searchResults.appendTo($body).show();
+//     }
   
-    $searchBox.on('submit', function(event) {
-      event.preventDefault();
-      $searchInput.trigger('input');
-    });
+//     $searchBox.on('submit', function(event) {
+//       event.preventDefault();
+//       $searchInput.trigger('input');
+//     });
   
-    $searchIcon.on('click', function(event) {
-      event.preventDefault();
-      $searchBox.toggle();
-      $searchInput.focus();
-    });
+//     $searchIcon.on('click', function(event) {
+//       event.preventDefault();
+//       $searchBox.toggle();
+//       $searchInput.focus();
+//     });
   
-    $(document).on('click', function(event) {
-        if (!$(event.target).closest('.search-box, .search-icon').length) {
-            $searchBox.hide();
-            $searchResults.hide();
-        } 
-    });
-  });
+//     $(document).on('click', function(event) {
+//         if (!$(event.target).closest('.search-box, .search-icon').length) {
+//             $searchBox.hide();
+//             $searchResults.hide();
+//         } 
+//     });
+//   });
